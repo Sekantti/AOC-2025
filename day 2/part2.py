b74cf1a7575338660949808803e2c5a145571c87
Sekantti <k.randomletters@gmail.com>
2025-12-13 11:04:06 +0100

    day 2

--- day 2/part2.py
+++ day 2/part2.py
@@ -17,31 +17,31 @@ def getRanges(file):
 def get_digits(number, n, pow):
     return number // 10**n % 10**pow
 
-def isNumRepeating(int, mod):
+def isNumRepeating(int, repeats):
     length = len(str(int))
-    if length % mod == 0:
-        if get_digits(int, 0, length//mod) == get_digits(int, length//mod, length//mod):
-            return True
+    if length % repeats == 0:
+        repeated_length = length//repeats
+        pattern = get_digits(int, 0, repeated_length)
+        for x in range(1,repeats):
+            new_pattern = get_digits(int, repeated_length*x, repeated_length)
+            if pattern != new_pattern:
+                return False
+        return True
     return False
-
-""" def isNumRepeating(int):
-    length = len(str(int))
-    if length % 2 == 0:
-        if get_digits(int, 0, length//2) == get_digits(int, length//2, length//2):
-            return True
-
-    return False """
 
 def check_ranges(file):
     ranges = getRanges(file)
     invalidIDs = 0
     for IDrange in ranges:
-        for x in range(IDrange[0], IDrange[1]):
-            if isNumRepeating(x, 2):
-                invalidIDs += x
+        for x in range(IDrange[0], IDrange[1]+1):
+            length = len(str(x))
+            seen = False
+            for y in range(2, length+1):
+                if seen == False:
+                    if isNumRepeating(x, y):
+                        invalidIDs+=x
+                        seen = True
 
     return invalidIDs
 
-#print(getRanges("example.txt"))
-#print(isNumRepeating(2020))
 print(check_ranges("input.txt"))
